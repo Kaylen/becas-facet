e844991d707fb7094c98f5df1401ae555dea661b
Miguel Liezun <liezun.js@gmail.com>
2017-02-25 18:34:52 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+from urllib.request import urlopen
+from dateutil.parser import parse as dateParser
+
+def main():
+    #Reading raw data from website
+    url = "http://www.facet.unt.edu.ar/facetinforma/category/becas/"
+    data = urlopen(url)
+    text = data.read().decode('utf-8')
+
+    #Getting ISO 8601 format date
+    text = text[text.find("datetime"):text.find("</time")]
+    text = text[text.find("\"")+1:text.find("\">")]
+
+    #Datetime object
+    date = dateParser(text)
+
+    #Checks last date. If it's the first time sets the last date
+    try:
+        f = open("last","r")
+    except FileNotFoundError:
+        f = open("last","w")
+        f.write(text)
+        f.close()
+        f = open("last","r")
+
+    lastdate = dateParser(f.read())
+
+    if date > lastdate:
+        print("New beca")
+        f = open("last","w")
+        f.write(text)
+        f.close()
+
+if __name__ == "__main__":
+    main()
